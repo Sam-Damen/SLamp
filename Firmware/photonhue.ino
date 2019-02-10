69bd03630f19c0927e6d264b70217330304c53e1
Sam-Damen <sam.damen.93@gmail.com>
2019-02-10 04:40:53 +0000

    Add files via upload
    Includes alarm firmware

--- Firmware/photonhue.ino
+++ Firmware/photonhue.ino
@@ -1,11 +1,7 @@
 SYSTEM_THREAD(ENABLED);
 
-#include "FastLED/FastLED.h"
+#include <FastLED.h>
 FASTLED_USING_NAMESPACE;
-
-#if FASTLED_VERSION < 3001000
-#error "Requires FastLED 3.1 or later; check github for latest code."
-#endif
 
 // UDP SETTINGS
 #define SERVER_PORT 49692
@@ -48,6 +44,13 @@ unsigned long smoothMillis;
 
 //EXTRA FUNCTIONS
 unsigned int partyMode = 0;
+unsigned int wakeupAlarm = 0;
+long alarm = 0;
+#define ALARM_LEAD_TIME 5
+
+byte redA = 182;
+byte greenA = 126;
+byte blueA = 91;
 
 
 void setup()
@@ -55,6 +58,17 @@ void setup()
     // WiFi
     lastWiFiCheck = millis();
     initWiFi();
+    
+    //Time and Daylight Saving
+    if (isDST()) {
+        Time.zone(11);
+    } else {
+        Time.zone(10);
+    }
+    
+    //Debugging
+    Serial.begin(9600);
+    
         
     // Leds - choose one correction method
     
@@ -122,7 +136,9 @@ void loop(){
             // Command options
             // 1 = force off
             // 2 = use lamp smoothing and validate by Orb ID
+            // 3 = Party Mode
             // 4 = validate by Orb ID
+            // 5 = Flag Wakeup Alarm
             // 8 = discovery
             if(commandOptions == 1)
             {
@@ -149,8 +165,13 @@ void loop(){
                 partyMode = 0;
                 setSmoothColor(red, green, blue);
                 
+                //Set colours for next alarm to show
+                redA = red;
+                greenA = green;
+                blueA = blue;
                 
-            } else if (commandOptions == 3) 
+            }
+            else if (commandOptions == 3) 
             {
                 partyMode = 1;
             }
@@ -167,6 +188,21 @@ void loop(){
 				
 				return;
             }
+            else if (commandOptions == 5)
+            {
+                //Rebuild the alarm time
+                alarm = (((long) red & 0xFF)) + (((long) green & 0xFF) << 8) + (((long) blue & 0xFF) << 16) + (((long) rcvOrbID & 0xFF) << 24);
+                
+                //Toggle the Alarm on and Off with Switch
+                if ( (Time.now() / 60) >= (alarm - ALARM_LEAD_TIME) ) {
+                    wakeupAlarm = 0;
+                } else {
+                    wakeupAlarm ^= 1;
+                }
+                
+                Serial.println(wakeupAlarm);
+
+            }
             else if(commandOptions == 8)
             {
                 // Respond to remote IP address with Orb ID
@@ -181,9 +217,18 @@ void loop(){
             }
         }
         
-    }else if(packetSize > 0){
+    } else if(packetSize > 0){
         // Got malformed packet
     }
+    
+    //Handle Wakeup Alarm
+    if (wakeupAlarm == 1) {
+        if ( (Time.now() / 60) >= (alarm - ALARM_LEAD_TIME) ) {
+            //Turn on the previous colour
+            setColor(redA,greenA,blueA);
+        }
+    }
+    
     
     if (partyMode == 0) {
     
@@ -198,12 +243,8 @@ void loop(){
         byte blue = random(255);
         
         setColor(red,green,blue);
-        //Causing problems with recieving data? 
+        //Causing problems with recieving data? TODO
         delay(random(100,1000));
-        //CHECK IF THIS WORKS!
-        if (client.parsePacket() == BUFFER_SIZE) {
-            partyMode = 0;
-        }
     }
 }
 
@@ -215,6 +256,7 @@ void setColor(byte red, byte green, byte blue)
         leds[i] = CRGB(red, green, blue); 
     }
     
+    Serial.println(red);
     FastLED.show();
 }
 
@@ -266,4 +308,37 @@ void clearSmoothColors()
     memset(prevColor, 0, sizeof(prevColor));
     memset(currentColor, 0, sizeof(nextColor));
     memset(nextColor, 0, sizeof(nextColor));
+}
+
+//Check if DST
+bool isDST()
+{
+    int month = Time.month();
+    int day = Time.day();
+    int dow = Time.weekday();
+    
+    //Get the date of the previous Sunday
+    int prevSun = day - dow;
+    
+    //May to September are not DST
+    if ( (month > 4) && (month < 10) ) {
+        return false;
+    }
+    
+    //November to March are DST
+    if ( (month > 10)  || (month < 4) ) {
+        return true;
+    }
+    
+    //After 1st Sunday in April is not DST
+    if (month == 4) {
+        return (prevSun <= 0);
+    }
+    
+    //After 1st Sunday in October is DST
+    if (month == 10) {
+        return (prevSun >= 0);
+    }
+    
+    return false;
 }
